5f0b28e3bce8cd422adbaf1e6aebded39e6f3ed1
madhav09 <madhavmishra083@gmail.com>
2025-12-23 23:44:56 +0530

    Started creating navigation bar for the page

--- src/nav.jsx
+++ src/nav.jsx
@@ -1,9 +1,17 @@
-import {useState} from "react";
-export default function ChatLine(){
-    const [isOpen, setIsOpen]=useState(false);
-    return(
+import { useState } from "react";
+export default function ChatLine() {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
         <>
-            
+            <nav className="font-robotomono text-white">
+                <h1 className="text-[35px]">SafePlace</h1>
+                <ul>
+                    <li><a href="./login.jsx">Login</a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
         </>
     )
 }
