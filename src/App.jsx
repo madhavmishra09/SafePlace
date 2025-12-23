1edc45e3e385a7bacc61d9fe622d55a58125a6a1
madhav09 <madhavmishra083@gmail.com>
2025-12-23 22:38:59 +0530

    Adding the project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './nav.jsx'
+import { useNavigate,Routes,Route } from 'react-router';
+function App() {
+  const [count, setCount] = useState(0)
+  const [loading,setLoading]=useState(false);
+  const navigate=useNavigate();
+  return (
+    <>
+      <Routes>
+        <Route path="/"
+        element={
+          <div><NavBar/></div>
+        }
+      />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/nav.jsx
@@ -0,0 +1,7 @@
+import {useState} from "react";
+export default function Navbar(){
+    const [isOpen, setIsOpen]=useState(false);
+    return{
+        <nav></nav>
+    }
+}
