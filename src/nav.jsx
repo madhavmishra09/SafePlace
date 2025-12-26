ad4ca4b1c0400c7d3e60201e862ef3df5e1e49a0
madhav09 <madhavmishra083@gmail.com>
2025-12-26 21:23:18 +0530

    Navigation bar is almost complete

--- src/nav.jsx
+++ src/nav.jsx
@@ -3,7 +3,7 @@ export default function ChatLine() {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <>
-            <nav className="font-robotomono text-white p-[1px] m-[1px] flex flex-row justify-evenly">
+            <nav className="font-robotomono text-white pt-[10px] m-[1px] flex flex-row justify-evenly bg-slate-900 h-[90px] w-250 rounded-[30px]">
                 <h1 className="text-[45px]">SafePlace</h1>
                 <ul className="flex flex-row p-[10px] m-[1px]">
                     <li className="w-[150px] h-[200px]"><a href="./login.jsx" className="bg-white border-slate-700 text-[30px] text-slate-700 hover:bg-slate-700 hover:text-white duration-250 rounded-[30px] flex justify-evenly">Login</a></li>
