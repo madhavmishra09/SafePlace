29887c1b7469a2fd22d75fe91a7743288daf113f
madhav09 <madhavmishra083@gmail.com>
2026-01-03 20:01:09 +0530

    I have just centered the message
    hello

--- src/chat interface/chatlandingpage.jsx
+++ src/chat interface/chatlandingpage.jsx
@@ -3,7 +3,7 @@ export default function ChatLandingPage(){
     const [isOpen, setIsOpen]=useState(false);
     return(
         <>
-            <section className="font-robotomono text-"><p>Hello, how are you doing today?</p></section>
+            <section className="font-robotomono text-slate-100 font-bold text-center text-[50px] m-10 p-10 flex justify-center items-center min-h-screen"><p>Hello, how are you doing today?</p></section>
         </>
     )
 }
