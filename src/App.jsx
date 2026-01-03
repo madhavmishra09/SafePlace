c27857447cb6f79ec94784cdf4e1f70b2608c4c8
madhav09 <madhavmishra083@gmail.com>
2026-01-03 19:39:47 +0530

    I have just updated the imports

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from './components/nav.jsx'
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './auth/login';
 import SignUp from './auth/signup';
+import ChatPage from './chat interface/chat.jsx'
 function App() {
 
   return (
@@ -20,6 +21,7 @@ function App() {
         />
         <Route path="/login" element={<><div><LoginPage /></div></>} />
         <Route path="/signup" element={<><div><SignUp /></div></>} />
+        <Route path="/chat" element={<><div><ChatPage /></div></>} />
       </Routes>
   )
 }
