955509627a1fa9afaa1cfe7040f63f2f9b28cb7d
madhav09 <madhavmishra083@gmail.com>
2026-01-25 23:26:06 +0530

    Fixed the errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,24 @@ import NavBar from './components/nav.jsx'
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './auth/login';
 import SignUp from './auth/signup';
-import ChatPage from './chat interface/layout/ChatLayout.jsx'
+import ChatPage from './chat-interface/layout/ChatLayout.jsx'
+
 function App() {
 
   return (
-
-      <Routes>
-        <Route path="/"
-          element={
-            <>
-              <div><NavBar /></div>
-              <div><Landing /></div>
-            </>
-          }
-        />
-        <Route path="/login" element={<><div><LoginPage /></div></>} />
-        <Route path="/signup" element={<><div><SignUp /></div></>} />
-        <Route path="/chat" element={<><div><ChatPage /></div></>} />
-      </Routes>
+    <Routes>
+      <Route path="/"
+        element={
+          <>
+            <div><NavBar /></div>
+            <div><Landing /></div>
+          </>
+        }
+      />
+      <Route path="/login" element={<><div><LoginPage /></div></>} />
+      <Route path="/signup" element={<><div><SignUp /></div></>} />
+      <Route path="/chat" element={<><div><ChatPage /></div></>} />
+    </Routes>
   )
 }
 
